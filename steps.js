e147dee9d10e46cb5d7b77e482c258d6ef3a59f0
mdbappa0001 <abutanif366@gmail.com>
2022-04-20 20:41:19 +0600

    steps added

---
+++ steps.js
@@ -0,0 +1,27 @@
+/* 
+-------------------------
+BASIC server SETUP
+-------------------------
+* 1. create a folder(manually or mkdir)
+* 2. opne command link to that folder
+3. npm init
+4. npm install express
+--------------------------
+For Express/ node server
+-----------------------------
+1. require express
+2. create app variable
+3. declare port
+4. set app.get('/')
+5. listen to port
+6. node index.js
+7. check your browser for that port
+---------------------------------
+get
+-------------------------------
+1. cors middleware
+-------------------------------
+post
+-------------------------------
+1.
+*/
